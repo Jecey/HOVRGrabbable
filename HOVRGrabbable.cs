66f1a3d5276a971e344e900a7d0797ef5790d69e
Jeroen Ceyssens <jecey96@gmail.com>
2022-01-16 16:40:51 +0100

    Create HOVRGrabbable README instructions

---
+++ HOVRGrabbable.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+/*
+ * Author: Jeroen Ceyssens (https://github.com/Jecey)
+ * See LICENSE file to see availability of this script
+ */
+public class HOVRGrabbable : OVRGrabbable
+{
+    private Transform _snapTransform;
+    private bool _shouldSnapPos;
+    private bool _shouldSnapRot;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+
+        // Set own custom values equal to the inspector values of OVRGrabbable
+        _snapTransform = m_snapOffset;
+        _shouldSnapPos = snapPosition;
+        _shouldSnapRot = snapOrientation;
+
+        // Disable the editor values after copying to stop the base class from carrying out
+        // the procedures
+        m_snapOffset = null;
+        m_snapOrientation = false;
+        m_snapPosition = false;
+    }
+
+    // Override the GrabBegin call of OVRGrabbable and utilize the custom snapping mechanic.
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        if (_snapTransform)
+        {
+            if (_shouldSnapRot)
+            {
+                Quaternion handRotation = hand.transform.rotation;
+                if (hand.name.ToLower().Contains("left")) // Flip the Z axis to give the same results as the right hand
+                    handRotation.eulerAngles = new Vector3(handRotation.eulerAngles.x, handRotation.eulerAngles.y, 180 + handRotation.eulerAngles.z);
+                this.transform.rotation = handRotation * Quaternion.Inverse(Quaternion.Inverse(this.transform.rotation) * _snapTransform.rotation);
+            }
+            if (_shouldSnapPos)
+            {
+                this.transform.position = hand.transform.position;
+                Vector3 snapPoint = _snapTransform.position;
+                if (hand.name.ToLower().Contains("left")) // Flip the Y value due to flipped Z axis
+                   snapPoint = this.transform.TransformPoint(new Vector3(_snapTransform.localPosition.x, -_snapTransform.localPosition.y, _snapTransform.localPosition.z));
+                Vector3 diff = hand.transform.position - snapPoint;
+                this.transform.position += diff;
+            }
+
+        }
+        base.GrabBegin(hand, grabPoint);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # HOVRGrabbable
 This repository contains a wrapper around the OVRGrabbable script of Oculus to make snap offset mechanics more intuitive towards developers.
+
+## How it works
+To ensure the grab will work correctly, enable the properties "Parent Held Object" and "Move Hand Position" on all OVRGrabbers (typically your right and left hand).
+![Setting properties Parent Held Object and Move Hand Position](./res/ovrgrabber_config.png) 
+
+Then to create a new snap spot for objects do the following:
+- Create a new empty GameObject (this will be the *Snap Transform*), we will call it "GrabPoint" in the screenshots.
+- Copy the right hand asset in the scene (in our case *CustomHandRight* from Oculus OVR package).
+- Move and rotate the hand object to the orientation and location where you want it to grab the object.
+![CustomHandRight copy in scene](./res/customhandright_copy.png)
+- Copy the values of the hand transform to the new GameObject ("GrabPoint").
+- Drag the new GameObject ("GrabPoint") so it becomes the child of the object you want to grab (in our case the object "Bat_low").
+![GrabPoint as child object of Bat_low](./res/grabpoint_as_child.png)
+- Delete the copied hand asset from the scene("CustomHandRight").
+- Finally, set the *Snap Transform* of *HOVRGrabbable* in the object to grab ("Bat_low") to the new child object ("GrabPoint"), 
+![Snap values of HOVRGrabbable](./res/hovrgrabbable_snap.png)
+
+If you want the object to be grabbed to snap to the specific position, enable *Snap Position*. If you want the rotation to snap to the hand, enable *Snap Orientation*.
+
+## Why we do this
+The original [OVRGrabbable](https://developer.oculus.com/reference/unity/1.43/class_o_v_r_grabbable/) script requires developers to make the *Snap Transform* a child object of the VR hands. This creates a lot of clutter when there are a lot of objects that use these features and is less flexible during scene changes. It also requires more work to calculate the offset required to hold the objects properly.
